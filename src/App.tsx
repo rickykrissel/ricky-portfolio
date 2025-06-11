57a9c71609f95a691e9ec9d8c68279a48905601b
Ricky Krissel <rickyk06@outlook.com>
2025-06-11 11:43:51 -0400

    finish homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem} from '@chakra-ui/react'
+import { Grid, GridItem, Stack, Text, Image, HStack} from '@chakra-ui/react'
 import './App.css'
 import MenuBar from './assets/components/MenuBar'
 function App() {
@@ -9,7 +9,19 @@ function App() {
         <MenuBar></MenuBar>
       </GridItem>
       <GridItem area='main' borderTopWidth="2px">
-        Main
+        <HStack paddingTop={170} justifyContent='center'>
+        <Stack>
+          <Text paddingRight={95} fontFamily='ui-rounded' fontSize='5xl'>Ricky Krissel</Text>
+          <Text paddingRight={175} fontFamily='ui-rounded' fontSize='2xl'>Software Engineer</Text>
+          <ul>
+            <li>Based in Florida</li>
+            <li>Currently studying at Florida State University</li>
+          </ul>
+          
+        </Stack>
+        <Image width={400}  src='./src/assets/components/computerimg.png'></Image>
+        </HStack>
+        
       </GridItem>
     </Grid>
   )
