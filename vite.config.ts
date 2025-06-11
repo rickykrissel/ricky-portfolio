8d26f114ab671c102d0a6af36673df7d3e7b94a8
Ricky Krissel <rickyk06@outlook.com>
2025-06-11 16:28:37 -0400

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+  rollupOptions: {
+  input: {
+  home: '/index.html',
+  aboutMe: '/about-me.html',
+  contactMe:'/contact-me.html',
+  projects:'/projects.html'
+  },
+  },
+  },
+  });
