1bada6c3f0c2ebc082cb781a49ff78ddcb5f2044
Ricky Krissel <rickyk06@outlook.com>
2025-06-11 12:11:52 -0400

    finish about page

--- src/appsforadditionalpages/aboutapp.tsx
+++ src/appsforadditionalpages/aboutapp.tsx
@@ -1,5 +1,5 @@
-import { Grid, GridItem} from '@chakra-ui/react'
-
+import { Grid, GridItem, Stack, Text } from '@chakra-ui/react'
+import '../aboutapp.css'
 import MenuBar from '../assets/components/MenuBar'
 function AboutApp() {
 
@@ -8,8 +8,16 @@ function AboutApp() {
       <GridItem area='nav' paddingBottom='5px'>
         <MenuBar></MenuBar>
       </GridItem>
-      <GridItem area='main' borderTopWidth="2px">
-        About me
+      <GridItem  paddingTop={300}area='main' borderTopWidth="2px">
+        <Stack paddingLeft={520} width={1400}>
+        <p>
+          Hello, my name is Ricky Krissel. Computer science and programming have been  interests of mine throughout my highschool and college education. I am currently furthering this passion by pursuing a bachelor's degree in Computer Science at Florida State University.
+        </p>
+        <br></br>
+        <p>
+           My particular areas of interest within the broader computer science field include software engineering and machine learning, and I am actively seeking out roles in these specialties. However, I still have much to learn and am open to a variety of opportunities across the field of Computer Science. Please head to the contact me page to reach out.
+        </p>
+        </Stack>
       </GridItem>
     </Grid>
   )
