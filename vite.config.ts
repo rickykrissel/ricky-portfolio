554ebe0a53dbdad106148d9c24591d7da59bae82
Ricky Krissel <rickyk06@outlook.com>
2025-06-13 14:03:35 -0400

    possible config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-  rollupOptions: {
-  input: {
-  home: '/index.html',
-  aboutMe: '/about-me.html',
-  contactMe:'/contact-me.html',
-  projects:'/projects.html'
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname,'index.html'),
+        aboutMe: resolve(__dirname, 'about-me.html'),
+        contactMe: resolve(__dirname,'contact-me.html'),
+        projects: resolve(__dirname,'projects.html')
+      },
+    },
   },
-  },
-  },
-  });
+})
